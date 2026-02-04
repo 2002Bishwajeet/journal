7eadfc7fd0e8d05f4a638c837e9308360ede0331
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2026-02-04 16:59:13 +0100

    feat: new hooks (future scope)

---
+++ src/hooks/circles/useCircles.ts
@@ -0,0 +1,25 @@
+import { QueryClient, useQuery } from '@tanstack/react-query';
+import { getCircles } from '@homebase-id/js-lib/network';
+import { useDotYouClientContext } from '@/components/auth';
+
+
+export const useCircles = (excludeSystemCircles = false) => {
+    const dotYouClient = useDotYouClientContext();
+
+    const fetchAll = async () => {
+        const circles = await getCircles(dotYouClient, excludeSystemCircles);
+        return circles?.sort((a, b) => (a.disabled ? 1 : 0) - (b.disabled ? 1 : 0));
+    };
+
+    return {
+        fetch: useQuery({
+            queryKey: ['circles', excludeSystemCircles],
+            queryFn: () => fetchAll(),
+            refetchOnWindowFocus: false,
+        }),
+    };
+};
+
+export const invalidateCircles = (queryClient: QueryClient) => {
+    queryClient.invalidateQueries({ queryKey: ['circles'], exact: false });
+};
