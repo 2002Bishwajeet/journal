69af54abf422aff2f3f34ea49fb925dd3065ad3f
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2026-03-25 18:01:00 +0100

    feat: add WebLLM model registry with Qwen2.5 and SmolLM2 options

--- src/lib/webllm/index.ts
+++ src/lib/webllm/index.ts
@@ -11,3 +11,8 @@ export {
 } from './engine';
 
 export type { RewriteStyle, ChatMessage } from './engine';
+
+export { AVAILABLE_MODELS, DEFAULT_MODEL_ID, getModelInfo, type ModelInfo } from './models';
+
+// Will be available after engine.ts update (Task 4):
+// export { getCurrentModelId, setIdleTimeout } from './engine';
